4e193b65a382d85c826b66f30aa845abeffe9e69
AbayR <62003749+AbayR@users.noreply.github.com>
2022-02-24 16:58:54 +0600

    Add files via upload

--- src/service/IEmployeeService.java
+++ src/service/IEmployeeService.java
@@ -7,9 +7,13 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface IEmployeeService {
+    /*this class was created to connect the db with main, the encapsulation is going to be done in other classes
+    * this class is important because for example if we want to make changes in db for example to move from the mysql to postgre or oracle
+    * all we need to change is this interface, so we dont need to change whole main class, it was possible using polymorphism
+    *  */
     public abstract void addEmployee(Employee e);
     public abstract void deleteEmployee(int empid);
     public abstract void updateEmployee(Employee e);
-    public abstract Employee findEmployee(Employee e) throws EmployeeNotFoundException, SQLException;
+    public abstract Employee findEmployee(Employee e) throws EmployeeNotFoundException, SQLException; /*connecting the database with the main class*/
     public List<Employee> showAllEmployees();
 }
